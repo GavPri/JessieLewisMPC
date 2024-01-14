4d8a7cac4f67656fb5b1b2e6648e8ba82fce9789
德华 刘 <gavinprior5@gmail.com>
2024-01-14 20:52:23 +0000

    add inital styling to header

--- jessie-lewis/src/components/navigation-bar/Header.js
+++ jessie-lewis/src/components/navigation-bar/Header.js
@@ -3,7 +3,7 @@ import Logo from "../Logo";
 
 const Header = () => {
   return (
-    <header>
+    <header className="bg-light sticky top-2 m-auto max-w-[1240px] min-w-[280px]">
       {/* Logo Div */}
       <Logo height={45} />
       {/* End Of Logo Div */}
