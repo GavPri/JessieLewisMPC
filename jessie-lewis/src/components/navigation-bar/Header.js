b1a2d1c9536c3e2b6799cd4c7734a2c3c91f553a
德华 刘 <gavinprior5@gmail.com>
2024-01-14 21:30:36 +0000

    color changes to body and header

--- jessie-lewis/src/components/navigation-bar/Header.js
+++ jessie-lewis/src/components/navigation-bar/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-light sticky top-2 m-auto max-w-[1240px] min-w-[280px] h-24 flex justify-between items-center p-4">
+    <header className="bg-neutral rounded-md sticky mt-2 m-auto max-w-[1240px] min-w-[280px] h-24 flex justify-between items-center p-4">
       {/* Logo Div */}
-      <Logo height={45} />
+      <Logo height={100} />
       {/* End Of Logo Div */}
       <nav>
         <MobileLinks
