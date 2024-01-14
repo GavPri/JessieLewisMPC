c9ab42ea46a19214bc4f66f51207bd220acef7c0
德华 刘 <gavinprior5@gmail.com>
2024-01-14 22:36:41 +0000

    add navlink to logo

--- jessie-lewis/src/components/Logo.js
+++ jessie-lewis/src/components/Logo.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Logo = ({ height = 45 }) => {
   return (
-    <img
-      src="/JessieLewis.png"
-      alt="Logo for Jessie Lewis Mental Performance Consulting"
-      height={height}
-      width={height}
-    />
+    <NavLink to="/">
+      <img
+        src="/JessieLewis.png"
+        alt="Logo for Jessie Lewis Mental Performance Consulting"
+        height={height}
+        width={height}
+      />
+    </NavLink>
   );
 };
 
--- jessie-lewis/src/components/navigation-bar/DesktopLinks.js
+++ jessie-lewis/src/components/navigation-bar/DesktopLinks.js
@@ -4,7 +4,9 @@ import { NavLink } from "react-router-dom";
 const DesktopLinks = () => {
   return (
     <ul className="hidden md:flex text-darkest">
-      <li className="mr-4">About</li>
+      <li className="mr-4">
+        <NavLink to="/about">About</NavLink>
+      </li>
       <li className="mr-4">Services</li>
       <li className="mr-4">Contact</li>
       <li className="mr-4">FAQs</li>
