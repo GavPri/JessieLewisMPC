93b122583412f82160ddc1617f0014145b27cd93
德华 刘 <gavinprior5@gmail.com>
2024-01-14 20:45:34 +0000

    create color pallete & import font

--- jessie-lewis/tailwind.config.js
+++ jessie-lewis/tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      darkest: "#545454",
+      dark: "#737373",
+      neutral: "#a6a6a6",
+      light: "#d9d9d9",
+      lightest: "#f9f9f9",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
