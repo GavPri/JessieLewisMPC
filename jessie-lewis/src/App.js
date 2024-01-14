889769ab26d518a71464b893cc60a2b0b61f4a25
德华 刘 <gavinprior5@gmail.com>
2024-01-14 22:04:53 +0000

    import switch and route to app.js

--- jessie-lewis/src/App.js
+++ jessie-lewis/src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/navigation-bar/Header";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="mt-24">
+        
+      </div>
     </div>
   );
 }
