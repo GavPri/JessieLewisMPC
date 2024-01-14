b3f348b7b7e180fed4ace521e3e7d2028c8aad09
德华 刘 <gavinprior5@gmail.com>
2024-01-14 20:33:06 +0000

    add logo to header

--- jessie-lewis/src/components/navigation-bar/Header.js
+++ jessie-lewis/src/components/navigation-bar/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Logo from "../Logo";
 
 const Header = () => {
-  return <header>
-    {/* Logo Div */}
-
-    {/* End Of Logo Div */}
-  </header>;
+  return (
+    <header>
+      {/* Logo Div */}
+      <Logo src="/JessieLewis.png" />
+      {/* End Of Logo Div */}
+    </header>
+  );
 };
 
 export default Header;
