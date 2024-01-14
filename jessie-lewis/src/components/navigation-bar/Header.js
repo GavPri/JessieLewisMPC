18ca60379d64a082ab7d6ab418814b936013b651
德华 刘 <gavinprior5@gmail.com>
2024-01-14 20:56:50 +0000

    use state hook w/ handle click for mobile links

--- jessie-lewis/src/components/navigation-bar/Header.js
+++ jessie-lewis/src/components/navigation-bar/Header.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Logo";
 
 const Header = () => {
+  // Use state hook for displaying mobile nav links or desktop navlinks
+  const [showMobileLinks, setShowMobileLinks] = useState(false);
+  // handle mobile function to display links
+  const handleMobileLinks = () => {
+    setShowMobileLinks(!showMobileLinks);
+  };
+  
   return (
     <header className="bg-light sticky top-2 m-auto max-w-[1240px] min-w-[280px]">
       {/* Logo Div */}
