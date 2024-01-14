ac5e815da6eb669e1c35b3b20f1b9d7c124e2329
德华 刘 <gavinprior5@gmail.com>
2024-01-14 21:08:53 +0000

    conditional rendering of menu or close icons

--- jessie-lewis/src/components/navigation-bar/MobileLinks.js
+++ jessie-lewis/src/components/navigation-bar/MobileLinks.js
@@ -2,12 +2,18 @@ import React from "react";
 // Icons for hamburger menu
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
-const MobileLinks = () => {
+const MobileLinks = ({
+  showMobileLinks,
+  setShowMobileLinks,
+  handleMobileLinks,
+}) => {
   return (
-    <div>
-      <div>
-        <AiOutlineMenu size={40} />
-      </div>
+    <div onClick={handleMobileLinks}>
+      {showMobileLinks ? (
+        <AiOutlineClose size={20} />
+      ) : (
+        <AiOutlineMenu size={20} />
+      )}
     </div>
   );
 };
