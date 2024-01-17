93163325ca21f607c10477e41a8b4b03df4d1e13
德华 刘 <gavinprior5@gmail.com>
2024-01-17 12:53:16 +0000

    initial styling of button variants with getButtonVariant function

--- jessie-lewis/src/components/Button.js
+++ jessie-lewis/src/components/Button.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 const Button = ({ type, onClick, className, text, variant }) => {
+  const getButtonVariant = () => {
+    switch (variant) {
+      case "secondary":
+        return "bg-transparent font-poppins font-bold rounded-md text-darkest border-2 border-transparent hover:border-neutral hover:bg-darkest hover:text-light";
+      default:
+        return "bg-darkest text-light font-poppins font-bold rounded-md";
+    }
+  };
   return (
-    <button type={type} onClick={onClick} className={className}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`py-2 px-4 font-poppins font-bold ${getButtonVariant()} ${className}`}
+    >
       {text}
     </button>
   );
