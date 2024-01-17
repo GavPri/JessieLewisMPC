d16f4b610504a5f04c4a20f7acc352b1ae6e50ec
德华 刘 <gavinprior5@gmail.com>
2024-01-17 21:30:15 +0000

    pull data and style services section

--- jessie-lewis/src/components/Button.js
+++ jessie-lewis/src/components/Button.js
@@ -5,16 +5,16 @@ const Button = ({ type, onClick, className, text, variant, icon, to }) => {
   const getButtonVariant = () => {
     switch (variant) {
       case "secondary":
-        return "m-2 bg-transparent font-poppins font-bold rounded-md text-cyanAccent border-2 border-transparent hover:border-lightest hover:bg-cyanAccent hover:text-lightest transition-all duration-500";
+        return " bg-lightest font-poppins font-bold rounded-md text-cyanAccent border-2 border-transparent hover:border-lightest hover:bg-cyanAccent hover:text-lightest transition-all duration-500";
       default:
-        return "m-2 bg-tealAccent text-lightest border-2 border-light font-poppins font-bold rounded-md hover:bg-lightest hover:text-tealAccent hover:border-2 hover:border-tealAccent ";
+        return " bg-tealAccent text-lightest border-2 border-light font-poppins font-bold rounded-md hover:bg-lightest hover:text-tealAccent hover:border-2 hover:border-tealAccent ";
     }
   };
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`flex justify-center items-center text-xl w-10/12 py-2 px-4 font-poppins font-bold mx-2 ${getButtonVariant()} ${className}`}
+      className={`flex justify-center items-center text-xl w-fit py-2 px-4 font-poppins font-bold  ${getButtonVariant()} ${className}`}
     >
       <NavLink to={to}>{text}</NavLink>
       {icon && <span className="ml-2 rotate-[-45deg]">{icon}</span>}
--- jessie-lewis/src/pages/homepage/ServicesIntro.js
+++ jessie-lewis/src/pages/homepage/ServicesIntro.js
@@ -1,14 +1,32 @@
 import React from "react";
 import servicesData from "../../data/services-data/servicesData";
+import Button from "../../components/Button";
 
 const ServicesIntro = () => {
   return (
-    <div className="max-w-[1240px] bg-lightest m-auto p-4 mb-2 rounded-md">
+    <div className="max-w-[1240px] bg-lightest m-auto p-6 mb-2 rounded-md">
       <div className="w-4/6 border-b-2 border-b-dark">
         <h3 className="font-poppins font-bold text-darkest pb-2 text-3xl">
           Services
         </h3>
       </div>
+      {servicesData.services.map((service, index) => (
+        <div
+          key={index}
+          className="text-lightest bg-tealAccent mt-4 rounded-lg p-4 border-2 border-transparent hover:border-tealAccent hover:text-darkest hover:bg-lightest transition-all duration-500"
+        >
+          <div className="flex justify-start items-center max-w-readable">
+            <h4 className="text-2xl font-poppins mb-2 uppercase">
+              {service.serviceName}
+            </h4>
+            <p className="hidden md:text-4xl md:mb-2 ml-auto md:order-1">
+              {service.servicesIcon}
+            </p>
+          </div>
+          <p className="mb-2">{service.shortServiceIntro}</p>
+          <Button to="/services" text="Learn More" />
+        </div>
+      ))}
     </div>
   );
 };
