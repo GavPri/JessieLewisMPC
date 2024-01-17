fe536af47be01d0e511f30905ec7d70b712b1f52
德华 刘 <gavinprior5@gmail.com>
2024-01-17 15:45:29 +0000

    create social media links component

---
+++ jessie-lewis/src/components/SocialMediaLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SocialMediaLinks = () => {
+  return (
+    <ul>
+      <li>Instagram</li>
+      <li>Facebook</li>
+      <li>Twitter</li>
+      <li>Pinterest</li>
+    </ul>
+  );
+};
+
+export default SocialMediaLinks;
