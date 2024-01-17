8e0b30d55542acf7b35987dfb63a38d84b9a134e
德华 刘 <gavinprior5@gmail.com>
2024-01-17 15:09:37 +0000

    add margin to navbar and round home page to create seperation

--- jessie-lewis/src/pages/HomePage.js
+++ jessie-lewis/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 const HomePage = () => {
   return (
-    <div className="min-h-fit p-4 bg-lightest min-w-screen md:max-w-[1240px] md:m-auto md:flex md:justify-between md:items-start">
+    <div className="min-h-fit p-4 rounded-md bg-lightest min-w-screen md:max-w-[1240px] md:m-auto md:flex md:justify-between md:items-start">
       <div className="md:rounded md:flex md:flex-col md:justify-evenly md:items-start">
         <h2 className="font-extrabold font-poppins text-3xl md:text-4xl  p-4 text-darkest">
           Mental Mastery For Peak Performance
