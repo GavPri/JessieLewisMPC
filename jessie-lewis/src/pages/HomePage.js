2db7d72aa3d677e9200f78c7226765706cc641ba
德华 刘 <gavinprior5@gmail.com>
2024-01-17 12:27:33 +0000

    style subtext

--- jessie-lewis/src/pages/HomePage.js
+++ jessie-lewis/src/pages/HomePage.js
@@ -3,10 +3,10 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-lightest min-w-screen">
-      <h2 className="font-bold font-poppins text-3xl text-dark w-2/3 p-4">
+      <h2 className="font-bold font-poppins text-3xl  w-2/3 p-4 text-darkest">
         Mental Mastery For Peak Performance
       </h2>
-      <p>
+      <p className="p-4 font-poppins text-dark w-5/6 font-bold">
         Peak performance is a mindset. Let us guide you through proven
         techniques to optimize your cognitive abilities and achieve your goals
       </p>
