cbaf567576c1f1bae527e5740365a7dd1daa4088
德华 刘 <gavinprior5@gmail.com>
2024-01-17 17:16:00 +0000

    mobile design for about me section

--- jessie-lewis/src/pages/homepage/AboutMeIntro.js
+++ jessie-lewis/src/pages/homepage/AboutMeIntro.js
@@ -8,33 +8,31 @@ import {
 
 const AboutMeIntro = () => {
   return (
-    <div className="max-w-[1240px] bg-lightest m-auto p-4 rounded-md md:flex  mb-2">
+    <div className="max-w-[1240px] bg-lightest m-auto p-4 rounded-md md:flex md:flex-row flex-col-reverse mb-2">
       {/* ImageDiv */}
       <div className="w-full md:basis-[33%] ">
         <img
           src="/AboutMeIntro.png"
           alt=""
-          className="h-full w-5/6 m-auto rounded-lg"
+          className="h-full w-5/6 md:w-full m-auto rounded-lg"
         />
       </div>
       {/* TextDiv */}
       <div className="flex flex-col">
         <div className="basis-[67%] p-6 flex flex-col">
-          <h3 className="text-darkest font-poppins font-bold ">About Me</h3>
+          <h3 className="text-darkest font-poppins font-bold text-3xl pb-2 border-b-2 mb-2  border-b-dark">
+            About Me
+          </h3>
           <p className="max-w-readable">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolores
-            facilis voluptates, ut animi impedit facere assumenda eos suscipit
-            maiores cum officia, eaque pariatur, asperiores labore cupiditate
-            dignissimos. Hic veritatis perspiciatis qui molestias rerum
-            reiciendis id sunt officiis, cum consequuntur maxime quaerat tempora
-            temporibus, dolorum doloribus, autem deserunt ipsa animi nisi
-            ratione laborum dolores possimus? Accusantium sapiente hic
-            recusandae, voluptatibus delectus alias quaerat asperiores
-            necessitatibus dolore vel fugit, excepturi pariatur quod, dolorem
-            amet nesciunt ipsa nulla ea aperiam! Nobis, porro!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            reiciendis debitis numquam veniam amet perspiciatis provident nemo
+            perferendis totam illo itaque fugit, architecto expedita facilis
+            recusandae doloremque cupiditate aspernatur? Mollitia esse
+            reprehenderit distinctio sed impedit, eum eaque dolorum nostrum
+            voluptatum!
           </p>
         </div>
-        <div className="flex w-full justify-between items-center p-6">
+        <div className="flex w-full justify-between items-center p-6 order-last">
           <Button
             to="/about"
             variant="secondary"
