e38937a457363551b88613865f3c6f3ea462c81f
德华 刘 <gavinprior5@gmail.com>
2024-01-17 12:13:05 +0000

    styling for homepage h2

--- jessie-lewis/src/pages/HomePage.js
+++ jessie-lewis/src/pages/HomePage.js
@@ -3,7 +3,10 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-lightest min-w-screen">
-      <h2>Mental Mastery For Peak Performance</h2>
+      <h2 className="font-bold font-poppins text-3xl text-dark w-2/3 p-4">
+        Mental Mastery For Peak Performance
+      </h2>
+      
     </div>
   );
 };
