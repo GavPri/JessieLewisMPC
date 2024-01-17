2f5fc8bcd54d16c4a2aed2bd64e400caf1c5d569
德华 刘 <gavinprior5@gmail.com>
2024-01-17 13:59:32 +0000

    add boilerplate styling for blur

---
+++ jessie-lewis/src/components/Blur.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blur = () => {
+  return (
+    <div className="absolute top-0 right-0">
+      <div className="w-4/6 aspect-square bg-gradient-to-r from-neutral to-dark opacity-50"></div>
+    </div>
+  );
+};
+
+export default Blur;
