ddd79ff4399b0ff66cbf01ea83b1bdeea37dac58
德华 刘 <gavinprior5@gmail.com>
2024-01-19 20:07:09 +0000

    home page styling

--- jessie-lewis/src/components/SocialMediaLinks.js
+++ jessie-lewis/src/components/SocialMediaLinks.js
@@ -8,23 +8,11 @@ import {
 
 const SocialMediaLinks = () => {
   return (
-    <ul className="w-5/6 flex justify-between font-poppins text-dark font-bold p-4">
+    <ul className="w-5/6 flex justify-between font-poppins text-light font-bold p-4">
       <li className="mr-2 flex justify-center items-center hover:text-tealAccent hover:cursor-pointer">
         Instagram
         <span className="ml-2">
           <AiOutlineInstagram />
-        </span>
-      </li>
-      <li className="mr-2 flex justify-center items-center hover:cursor-pointer hover:text-tealAccent">
-        Facebook
-        <span className="ml-2">
-          <AiOutlineFacebook />
-        </span>
-      </li>
-      <li className="mr-2 flex justify-center items-center hover:cursor-pointer hover:text-tealAccent">
-        Twitter
-        <span className="ml-2">
-          <AiOutlineTwitter />
         </span>
       </li>
     </ul>
