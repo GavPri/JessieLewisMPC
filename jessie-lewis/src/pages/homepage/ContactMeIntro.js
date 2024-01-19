7b4b3fdafdc7a4ec61968984719d8eb216c3b0dd
德华 刘 <gavinprior5@gmail.com>
2024-01-19 16:26:51 +0000

    create and render contact data in homepage

--- jessie-lewis/src/pages/homepage/ContactMeIntro.js
+++ jessie-lewis/src/pages/homepage/ContactMeIntro.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import contactData from "../../data/contact-data/contactData";
 const ContactMeIntro = () => {
   return (
-    <div><h1>Contact Me</h1></div>
-  )
-}
+    <div >
+      <h1>Contact Me</h1>
+      <div>
+        {contactData.meansOfContact.map((mean, index) => (
+          <>
+            <p>{mean.method} <span>{mean.methodIcon}</span></p>
+            <p>{mean.methodInfo}</p>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ContactMeIntro
+export default ContactMeIntro;
