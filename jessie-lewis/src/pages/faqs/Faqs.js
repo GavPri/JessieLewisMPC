f70fae8573631d98739e6413b75cfbb1e981dfdc
德华 刘 <gavinprior5@gmail.com>
2024-01-22 15:46:39 +0000

    adjust background of container

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -11,7 +11,7 @@ const Faqs = () => {
   };
 
   return (
-    <div className="mt-24 max-w-[1240px] m-auto bg-darkest h-fit p-6 md:flex md:justify-around rounded-md">
+    <div className="mt-24 max-w-[1240px] m-auto h-fit p-6 md:flex md:justify-around rounded-md">
       {/* image container */}
       <div
         className="hidden md:flex md:w-5/12 md:h-screen md:rounded-md"
@@ -19,10 +19,10 @@ const Faqs = () => {
       ></div>
       {/* text */}
       <div className="md:w-1/2 w-full text-center md:text-left md:flex md:flex-col">
-        <h1 className="font-poppins font-extrabold text-4xl text-lightest mb-2">
+        <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
           Frequently Asked Questions
         </h1>
-        <p className="text-light">Questions you may have about our services</p>
+        <p className="text-dark">Questions you may have about our services</p>
         <FaqsAccordion />
       </div>
     </div>
