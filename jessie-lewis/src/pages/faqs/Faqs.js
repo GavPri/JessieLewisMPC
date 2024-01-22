191c06857c39a099d1f65a47f52300f38e39d0bd
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:18:58 +0000

    wrap h1 and p tag of faqs in div

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -3,10 +3,12 @@ import React from "react";
 const Faqs = () => {
   return (
     <div className="mt-24 max-w-[1240px] m-auto">
-      <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
-        Frequently Asked Questions
-      </h1>
-      <p className="font-thin">Questions you may have about our services</p>
+      <div>
+        <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
+          Frequently Asked Questions
+        </h1>
+        <p className="font-thin">Questions you may have about our services</p>
+      </div>
     </div>
   );
 };
