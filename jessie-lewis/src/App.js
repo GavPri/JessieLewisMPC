7efbd06c1de3b61fd456b32bf6a259a7c41112e4
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:10:23 +0000

    import faq component to app.js

--- jessie-lewis/src/App.js
+++ jessie-lewis/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/navigation-bar/Header";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
+import Faqs from "./pages/faqs/Faqs";
 
 function App() {
   return (
@@ -16,11 +17,7 @@ function App() {
             render={() => <h1>Services Page!</h1>}
           />
           <Route exact path="/contact" render={() => <h1>Contact Page!</h1>} />
-          <Route
-            exact
-            path="/faqs"
-            render={() => <h1>Frequently Asked Questions!</h1>}
-          />
+          <Route exact path="/faqs" render={() => <Faqs />} />
         </Switch>
       </div>
     </div>
