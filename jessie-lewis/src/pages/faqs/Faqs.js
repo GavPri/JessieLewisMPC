88914304a4a5014802cab6611f767b0a4bbcd0fb
德华 刘 <gavinprior5@gmail.com>
2024-01-22 14:01:12 +0000

    styling for md screens and color change of wrapper & text

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -10,17 +10,18 @@ const Faqs = () => {
   };
 
   return (
-    <div className="mt-24 max-w-[1240px] m-auto bg-lightest h-screen">
+    <div className="mt-24 max-w-[1240px] m-auto bg-darkest h-screen p-6 md:flex md:justify-around rounded-md">
       {/* image container */}
-      <div className="hidden md:flex md:w-1/3 md:h-full" style={divStyle}></div>
+      <div
+        className="hidden md:flex md:w-5/12 md:h-full md:rounded-md"
+        style={divStyle}
+      ></div>
       {/* text */}
       <div className="w-full text-center">
-        <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
+        <h1 className="font-poppins font-extrabold text-4xl text-lightest mb-2">
           Frequently Asked Questions
         </h1>
-        <p className="text-darkest">
-          Questions you may have about our services
-        </p>
+        <p className="text-light">Questions you may have about our services</p>
       </div>
     </div>
   );
