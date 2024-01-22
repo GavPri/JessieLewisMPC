47fd565e9e4702752a21dfeab67fad2c493ea2e2
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:49:08 +0000

    import faq image to faqs.js

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -1,9 +1,11 @@
 import React from "react";
+import faqs from "../../images/faqs.jpeg";
 
 const Faqs = () => {
   return (
     <div className="mt-24 max-w-[1240px] m-auto bg-lightest">
-        {/* imaage container */}
+      {/* image container */}
+
       <div className="w-full text-center">
         <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
           Frequently Asked Questions
