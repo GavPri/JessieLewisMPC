06fdb50de687a719f525f86395a9effe094a6fa6
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:16:43 +0000

    add p tag with styling to faqs

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -3,9 +3,10 @@ import React from "react";
 const Faqs = () => {
   return (
     <div className="mt-24 max-w-[1240px] m-auto">
-      <h1 className="font-poppins font-extrabold text-2xl text-darkest">
+      <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
         Frequently Asked Questions
       </h1>
+      <p className="font-thin">Questions you may have about our services</p>
     </div>
   );
 };
