f80f8b2070cf6cc46e719b1328c7fe799b4e835a
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:14:05 +0000

    add styling to the h1 tag in faqs

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Faqs = () => {
-  return <div className="mt-24 max-w-[1240px] m-auto">Faqs</div>;
+  return (
+    <div className="mt-24 max-w-[1240px] m-auto">
+      <h1 className="font-poppins font-extrabold text-2xl text-darkest">
+        Frequently Asked Questions
+      </h1>
+    </div>
+  );
 };
 
 export default Faqs;
