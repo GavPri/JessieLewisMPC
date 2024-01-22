76a6bc89313cd347e554b20f62d8dcd7e62286b8
德华 刘 <gavinprior5@gmail.com>
2024-01-22 13:21:39 +0000

    adjust color of container and p tag in faqs

--- jessie-lewis/src/pages/faqs/Faqs.js
+++ jessie-lewis/src/pages/faqs/Faqs.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Faqs = () => {
   return (
-    <div className="mt-24 max-w-[1240px] m-auto">
+    <div className="mt-24 max-w-[1240px] m-auto bg-lightest">
       <div className="w-full text-center">
         <h1 className="font-poppins font-extrabold text-4xl text-darkest mb-2">
           Frequently Asked Questions
         </h1>
-        <p className="font-thin">Questions you may have about our services</p>
+        <p className="text-darkest">
+          Questions you may have about our services
+        </p>
       </div>
     </div>
   );
