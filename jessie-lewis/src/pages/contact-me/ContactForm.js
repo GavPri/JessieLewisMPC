a005232c133087c0cbb7dd5e19605000dc6d593b
德华 刘 <gavinprior5@gmail.com>
2024-01-24 20:59:06 +0000

    add email input field

--- jessie-lewis/src/pages/contact-me/ContactForm.js
+++ jessie-lewis/src/pages/contact-me/ContactForm.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContactForm = () => {
   return (
     <div className="max-w-[800px] bg-darkest m-auto mt-24">
-      <form className="max-w-[80%] m-auto">
+      <form className="max-w-[80%] m-auto p-6">
         <div className="max-w-full">
           <label htmlFor="name" className="hidden">
             Your full name:
@@ -11,6 +11,16 @@ const ContactForm = () => {
           <input
             type="text"
             placeholder="Your full name"
+            className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
+          />
+        </div>
+        <div className="max-w-full">
+          <label htmlFor="email" className="hidden">
+            Your email
+          </label>
+          <input
+            type="text"
+            placeholder="Email address"
             className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
           />
         </div>
