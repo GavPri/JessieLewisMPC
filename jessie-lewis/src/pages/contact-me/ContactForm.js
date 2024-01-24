0dabd029fb465537a6efae3dc550f63afe9ca930
德华 刘 <gavinprior5@gmail.com>
2024-01-24 20:47:20 +0000

    sizing and positioning for form element

--- jessie-lewis/src/pages/contact-me/ContactForm.js
+++ jessie-lewis/src/pages/contact-me/ContactForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div className="max-w-[800px] bg-darkest m-auto mt-24">
-   
-  </div>;
+  return (
+    <div className="max-w-[800px] bg-darkest m-auto mt-24">
+      <form className="max-w-[80%] m-auto">
+        
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
