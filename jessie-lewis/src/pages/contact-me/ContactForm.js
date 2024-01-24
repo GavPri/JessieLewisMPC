1f99aa9b4830dd49cd6a45ba60782f2b3a4c17eb
德华 刘 <gavinprior5@gmail.com>
2024-01-24 21:06:01 +0000

    add select option to form

--- jessie-lewis/src/pages/contact-me/ContactForm.js
+++ jessie-lewis/src/pages/contact-me/ContactForm.js
@@ -4,7 +4,7 @@ const ContactForm = () => {
   return (
     <div className="max-w-[800px] bg-darkest m-auto mt-24">
       <form className="max-w-[80%] m-auto p-6">
-        <div className="max-w-full">
+        <div className="max-w-full mb-4">
           <label htmlFor="name" className="hidden">
             Your full name:
           </label>
@@ -14,7 +14,7 @@ const ContactForm = () => {
             className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
           />
         </div>
-        <div className="max-w-full">
+        <div className="max-w-full mb-4">
           <label htmlFor="email" className="hidden">
             Your email
           </label>
@@ -23,6 +23,22 @@ const ContactForm = () => {
             placeholder="Email address"
             className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
           />
+        </div>
+        <div className="max-w-full mb-4">
+          <label htmlFor="email" className="hidden">
+            Service Enquiry
+          </label>
+          <select
+            name="services"
+            value="services"
+            className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
+          >
+            <option value="">Please select a topic</option>
+            <option value="1-1 coaching">1-1 coaching</option>
+            <option value="Teams & Organizations">Teams & Organizations</option>
+            <option value="Speaking Engagements">Speaking Engagements</option>
+            <option value="Other">Other</option>
+          </select>
         </div>
       </form>
     </div>
