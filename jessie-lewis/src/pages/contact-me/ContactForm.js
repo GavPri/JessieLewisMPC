f33e6e0b739f17855d5776950e886da2aa69fe69
德华 刘 <gavinprior5@gmail.com>
2024-01-24 20:57:10 +0000

    style input field

--- jessie-lewis/src/pages/contact-me/ContactForm.js
+++ jessie-lewis/src/pages/contact-me/ContactForm.js
@@ -4,11 +4,15 @@ const ContactForm = () => {
   return (
     <div className="max-w-[800px] bg-darkest m-auto mt-24">
       <form className="max-w-[80%] m-auto">
-        <div>
+        <div className="max-w-full">
           <label htmlFor="name" className="hidden">
             Your full name:
           </label>
-          <input type="text" placeholder="Your full name" />
+          <input
+            type="text"
+            placeholder="Your full name"
+            className="py-2 px-4 rounded-md font-poppins text-darkest min-w-full"
+          />
         </div>
       </form>
     </div>
