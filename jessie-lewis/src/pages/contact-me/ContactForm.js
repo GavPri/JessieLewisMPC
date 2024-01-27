754de2fa4c8c2a5f4dcc46155548844770d69c45
德华 刘 <gavinprior5@gmail.com>
2024-01-27 14:51:49 +0000

    copy code from email.js react docs

--- jessie-lewis/src/pages/contact-me/ContactForm.js
+++ jessie-lewis/src/pages/contact-me/ContactForm.js
@@ -1,7 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { AiOutlineMail } from "react-icons/ai";
 
 const ContactForm = () => {
+  // Email.js
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  // ----- End of email.js
   const [formData, setFormData] = useState({
     name: "",
     email: "",
