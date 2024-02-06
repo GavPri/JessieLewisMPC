9aab068fab32d1c8e155f05489e838b8d67b01ac
德华 刘 <gavinprior5@gmail.com>
2024-02-06 11:48:03 +0000

    import testimonial data to slider

--- jessie-lewis/src/pages/homepage/TestimonialSlider.js
+++ jessie-lewis/src/pages/homepage/TestimonialSlider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import testimonialData from "../../data/testimonial-data/TestimonialData";
 
 const TestimonialSlider = () => {
   return <div className="m-auto max-w-[1240px]">Testimonials</div>;
