4f635f2da1d284530c1ef6ed37ace54fb1b9f551
德华 刘 <gavinprior5@gmail.com>
2024-02-06 11:50:59 +0000

    create map function to display data

--- jessie-lewis/src/pages/homepage/TestimonialSlider.js
+++ jessie-lewis/src/pages/homepage/TestimonialSlider.js
@@ -2,7 +2,20 @@ import React from "react";
 import testimonialData from "../../data/testimonial-data/TestimonialData";
 
 const TestimonialSlider = () => {
-  return <div className="m-auto max-w-[1240px]">Testimonials</div>;
+  return (
+    <div className="m-auto max-w-[1240px]">
+      Testimonials
+      <div>
+        {testimonialData.map((t, index) => (
+          <>
+            <p>{t.name}</p>
+            <p>{t.role}</p>
+            <p> {t.testimonial} </p>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialSlider;
