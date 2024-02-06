fd7c3ad43fa2075d4e1bab4814f86716416a3d90
德华 刘 <gavinprior5@gmail.com>
2024-02-06 11:39:58 +0000

    add in name, role and testimonial into testimonialData

--- jessie-lewis/src/data/testimonial-data/TestimonialData.js
+++ jessie-lewis/src/data/testimonial-data/TestimonialData.js
@@ -1,3 +1,23 @@
-const testimonialData = [{}, {}, {}];
+const testimonialData = [
+  {
+    name: "John Doe",
+    role: "Tennis Coach",
+    testimonial:
+      "Jessie has been a pleasure to work with from the 1st time she came out to work with my high performance tennis juniors! She is insightful, compassionate and amazing as a mental coach. Anyone who gets the privilege of working with her will be the better for it!",
+  },
+  {
+    name: "Jessie Lewis",
+    role: "head women’s tennis coach at Cal State Northridge",
+    testimonial:
+      "Jessie Lewis is a game changer for our program.  Jessie is uniquely able to be a catalyst for improved performance while mentoring the whole person with a caring heart.  Jessie is a rising star that is smart, trustworthy, talented and compassionate to the needs of those in her charge.  Thank you Jessie for all you have done to make us better!",
+  },
+
+  {
+    name: "Sophie Marengo",
+    role: "Women’s soccer players at Cal State Northridge",
+    testimonial:
+      "Jessie is someone who made a significantly positive impact for our team! Jessie improved each individuals’ self-confidence on and off the field by using positive self-talk. In addition, she contributed positively towards our team helping each athletes’ performance anxiety using deep breathing strategies. Jessie is an individual who is very passionate on helping others become successful and is well-knowledged of improving one’s mental performance on and off the field.",
+  },
+];
 
 export default testimonialData;
