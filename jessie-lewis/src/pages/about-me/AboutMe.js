220e806e97041b3df3e5096438070a301f59b7fa
德华 刘 <gavinprior5@gmail.com>
2024-03-25 13:20:57 +0000

    import profile photo to AboutMe
    rename image name in images file

--- jessie-lewis/src/pages/about-me/AboutMe.js
+++ jessie-lewis/src/pages/about-me/AboutMe.js
@@ -1,7 +1,20 @@
 import React from "react";
+import jessiebanner from "../../images/jessiebanner.png";
 
 const AboutMe = () => {
-  return <div className="mt-24 max-w-[1240px] m-auto">Get to know Jessie</div>;
+  // ---- Profile picture
+  const divStyle = {
+    backgroundImage: `url(${jessiebanner})`,
+    backgroundSize: "cover",
+    backgroundPosition: "top, left",
+    backgroundRepeat: "no-repeat",
+  };
+
+  return (
+    <div className="mt-24 max-w-[1240px] m-auto">
+      Get to know Jessie
+    </div>
+  );
 };
 
 export default AboutMe;
