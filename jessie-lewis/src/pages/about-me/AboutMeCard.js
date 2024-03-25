79798d36f2118281b6839b5ceeb54d370971365a
德华 刘 <gavinprior5@gmail.com>
2024-03-25 15:29:45 +0000

    render card component on AboutMe.js

--- jessie-lewis/src/pages/about-me/AboutMeCard.js
+++ jessie-lewis/src/pages/about-me/AboutMeCard.js
@@ -1,31 +1,22 @@
 import React from "react";
 import aboutMeData from "../../data/aboutme-data/aboutMeData";
 
-const AboutMeCard = (props) => {
+const AboutMeCard = () => {
   return (
-    <div className="drop-shadow-xl border-2 border-lightest rounded-md max-w-[80vw] m-auto mt-4">
-      {/* ----- Title of card */}
-      <div>
-        {" "}
-        <h2 className=" text-xl font-bold bg-gradient-to-r from-darkest to-neutral text-transparent bg-clip-text uppercase ">
-          Education & Qualifications
-        </h2>
-        <p className="max-w-readable text-darkest mt-2 px-2">
-          Jessie holds a{" "}
-          <strong>
-            master's degree in clinical psychology from California State
-            University, Northridge,
-          </strong>{" "}
-          where she also completed her practicum hours as part of the
-          certification process.
-          <strong>
-            She earned a bachelor's degree in psychology from the University of
-            California, Berkeley
-          </strong>{" "}
-          , where she gained her initial experience in the field by teaching the
-          first student-led sport psychology course at UC Berkeley.
-        </p>
-      </div>
+    <div className="max-w-[80vw] m-auto mt-4">
+      {aboutMeData.map((t, idx) => (
+        <div
+          key={idx}
+          className="mb-4 text-left border-2 border-light rounded-md px-2"
+        >
+          <h2 className=" text-xl font-bold bg-gradient-to-r from-darkest to-neutral text-transparent bg-clip-text uppercase text-center">
+            {t.title}
+          </h2>
+          <p className="max-w-readable text-darkest mt-2 text-center pb-2">
+            {t.content}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
