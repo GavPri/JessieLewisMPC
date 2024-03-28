0b3e39e986dcd273e6cdb9d953bd5a7cedd2015b
德华 刘 <gavinprior5@gmail.com>
2024-03-28 18:37:24 +0000

    create div style for background image in services.js

--- jessie-lewis/src/pages/services/Services.js
+++ jessie-lewis/src/pages/services/Services.js
@@ -2,11 +2,20 @@ import React from "react";
 // ----- Hero Image
 import services from "../../images/services.jpeg";
 
-
 const Services = () => {
-  return <div className="max-w-[1240px] m-auto mt-24">
-    {/* ----- Banner image */}
-  </div>;
+  // ----- Style of div to hold background image
+  const divStyle = {
+    backgroundImage: `url(${services})`,
+    backgroundSize: "cover",
+    backgroundPosition: "top, left",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <div className="max-w-[1240px] m-auto mt-24">
+      {/* ----- Banner image */}
+      <div style={divStyle}></div>
+    </div>
+  );
 };
 
 export default Services;
