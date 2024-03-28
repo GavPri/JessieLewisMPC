51955180b174a0a937cb3a7c07038397d8e32a55
德华 刘 <gavinprior5@gmail.com>
2024-03-28 18:35:57 +0000

    import services.jpeg to services.js

--- jessie-lewis/src/pages/services/Services.js
+++ jessie-lewis/src/pages/services/Services.js
@@ -1,8 +1,11 @@
 import React from "react";
+// ----- Hero Image
+import services from "../../images/services.jpeg";
+
 
 const Services = () => {
   return <div className="max-w-[1240px] m-auto mt-24">
-
+    {/* ----- Banner image */}
   </div>;
 };
 
