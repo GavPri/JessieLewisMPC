c339a0a3ef127b4aaafe0824708288ca1b40853b
德华 刘 <gavinprior5@gmail.com>
2024-04-10 11:45:38 +0000

    remove some css from services ahead of redesign

--- jessie-lewis/src/pages/homepage/ServicesIntro.js
+++ jessie-lewis/src/pages/homepage/ServicesIntro.js
@@ -13,22 +13,11 @@ const ServicesIntro = () => {
       </div>
       <div className="lg:flex">
         {servicesData.services.map((service, index) => (
-          <div
-            key={index}
-            className="drop-shadow-lg overflow-hidden group bg-transparent sm:w-11/12 lg:mx-4 lg:flex lg:flex-col lg:justify-between lg:items-start text-darkest  mt-4 rounded-lg p-4 border-2 border-transparent hover:border-darkest hover:text-lightest glass hover:bg-darkest transition-all duration-500"
-          >
+          <div key={index}>
             <NavLink to="/services">
-              <div className="flex justify-start items-center max-w-readable ">
-                <h4 className="glass p-4 text-2xl font-poppins mb-2 uppercase">
-                  {service.serviceName}
-                </h4>
-                <p className="hidden lg:flex lg:text-4xl lg:mb-2 ml-auto lg:order-1 ">
-                  {service.servicesIcon}
-                </p>
-              </div>
-              <p className="lg:bg-darkest rounded-md mb-2 font-bold lg:text-lightest p-4 lg:transform lg:translate-x-full lg:group-hover:translate-x-0 lg:transition-transform lg:duration-500">
-                {service.shortServiceIntro}
-              </p>
+              <p className="">{service.servicesIcon}</p>
+              <h4 className="">{service.serviceName}</h4>
+              <p className="">{service.shortServiceIntro}</p>
             </NavLink>
             <Button to="/services" text="Learn More" />
           </div>
