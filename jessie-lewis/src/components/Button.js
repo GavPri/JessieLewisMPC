5e0062bdb241bc926793ad0022c43bc97d810865
德华 刘 <gavinprior5@gmail.com>
2024-04-10 12:09:43 +0000

    update default button component

--- jessie-lewis/src/components/Button.js
+++ jessie-lewis/src/components/Button.js
@@ -5,9 +5,9 @@ const Button = ({ type, onClick, className, text, variant, icon, to }) => {
   const getButtonVariant = () => {
     switch (variant) {
       case "secondary":
-        return " bg-lightest font-poppins font-bold rounded-md text-seafoam border-2 border-transparent hover:border-lightest hover:bg-seafoam hover:text-lightest transition-all duration-500";
+        return "bg-lightest font-poppins font-bold rounded-md text-seafoam border-2 border-transparent hover:border-lightest hover:bg-seafoam hover:text-lightest transition-all duration-500";
       default:
-        return " bg-seafoam text-lightest border-2 border-light font-poppins font-bold rounded-md hover:bg-lightest hover:text-seafoam hover:border-2 hover:border-seafoam transition-all duration-500";
+        return "py-2 px-4 rounded-md font-poppins hover:button-hover transition-all duration-500 button active:button-drop";
     }
   };
   return (
