dcccca2c14ea444279e12b732025b9414c82c5d5
德华 刘 <gavinprior5@gmail.com>
2024-04-10 12:43:39 +0000

    add about icon to map function

--- jessie-lewis/src/pages/about-me/AboutMeCard.js
+++ jessie-lewis/src/pages/about-me/AboutMeCard.js
@@ -6,7 +6,10 @@ const AboutMeCard = () => {
     <div className="">
       {aboutMeData.map((t, idx) => (
         <div key={idx} className="">
-          <h2 className="">{t.title}</h2>
+          <div >
+            <p>{t.aboutIcon}</p>
+            <h2 className="">{t.title}</h2>
+          </div>
           <div className="">
             <p className="">{t.content}</p>
           </div>
